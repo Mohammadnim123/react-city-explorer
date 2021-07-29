2e99ec5cfbc8042138b38c49a5abc8a5389fd350
Mohammadnim123 <mm.nemrawi96@gmail.com>
2021-07-30 01:55:01 +0300

    v

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Movie from './Movie'
+import { Container, Row ,Col} from 'react-bootstrap'
+
 export default function Movies(props) {
     
     return (
         <>
         <div className="movies-container" id='movies'>
         {props.moviesData.length && <h1>Movies</h1>}
-            
+        <Container>
+            <Row>
             {props.moviesData.map((elem, idx) => {
-                return <Movie
+                return <Col lg={4}> <Movie
+                
                 key = {idx}
                 released_on = {elem.released_on}
                 title= {elem.title}
@@ -16,9 +20,11 @@ export default function Movies(props) {
                 average_votes= {elem.average_votes}
                 popularity= {elem.popularity}
                 image_url = {elem.image_url}
-                />
+                /></Col>
 
             })}
+            </Row>
+            </Container>
         </div>
         </>
     )
