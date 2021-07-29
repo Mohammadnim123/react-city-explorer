8bc1d3fbea15bfa32890e128bc691ac316ce8c90
Mohammadnim123 <mm.nemrawi96@gmail.com>
2021-07-30 01:54:56 +0300

    v

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,11 @@ import { Card } from 'react-bootstrap'
 
 export default function Movie(props) {
     return (
-        <Card style={{ width: '18rem', marginTop: '10px' }}>
+        <Card style={{ width: '18rem', marginTop: '10px'  }}>
             <Card.Header>
                 <small className="text-muted">released on: {props.released_on}</small>
             </Card.Header>
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.image_url}`} />
+            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.image_url}`} style={{height:'429px', width:'286px'}} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text style={{ overflow: 'scroll', height: '200px' }}>
